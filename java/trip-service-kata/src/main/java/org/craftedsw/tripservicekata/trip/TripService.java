0882c860057587df2bd8329fce3ec636588210b8
Mayank Agrawal <mayankagrawal.main@gmail.com>
2025-06-18 01:21:55 +0530

    Extract the logic to find if two users are friends

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -12,21 +12,18 @@ public class TripService {
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
 		User loggedUser = loggedUser();
-		boolean isFriend = false;
 		if (loggedUser != null) {
-			for (User friend : user.getFriends()) {
-				if (friend.equals(loggedUser)) {
-					isFriend = true;
-					break;
-				}
-			}
-			if (isFriend) {
+			if (areFriends(user, loggedUser)) {
 				tripList = tripsBy(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
+	}
+
+	private static boolean areFriends(User user, User anotherUser) {
+		return user.getFriends().contains(anotherUser);
 	}
 
 	protected List<Trip> tripsBy(User user) {
