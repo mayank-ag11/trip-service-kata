e189c066c25a3c525fd3fad424ab2d5ef0c390f6
Mayank Agrawal <mayankagrawal.main@gmail.com>
2025-06-18 00:12:05 +0530

    Test if the user is not logged in

--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,24 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 public class TripServiceTest {
-	
+
+    @Test()
+    void shouldThrowAnException_whenUserIsNotLoggedIn() {
+        TripService tripServiceWithNullUser = new TripService() {
+            @Override
+            protected User getLoggedUser() {
+                return null;
+            }
+        };
+
+        assertThrows(UserNotLoggedInException.class, () -> {
+            tripServiceWithNullUser.getTripsByUser(null);
+        });
+    }
 }
